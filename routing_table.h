bd5d1c5b10820db6539c87acb6aa1572f3c90d69
444iamahmed <54366105+444iamahmed@users.noreply.github.com>
2020-11-19 21:20:32 +0500

    barebones routing data structures added

---
+++ routing_table.h
@@ -0,0 +1,7 @@
+#include <vector>
+#include "routing_tuple.h"
+using namespace std;
+struct routing_table
+{
+    vector<routing_tuple> table;
+};
---
+++ routing_tuple.h
@@ -0,0 +1,7 @@
+struct routing_tuple
+{
+    int destination_port;
+    int router_port;
+    int num_hops;
+    int path[2];
+};
