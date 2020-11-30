8f2a87411594fbe1eeeb7ac58d5a4709b2b5a9f6
khadija-bash <74656198+khadija-bash@users.noreply.github.com>
2020-11-30 16:31:24 +0500

    Add files via upload

---
+++ routing_table.h
@@ -0,0 +1,32 @@
+#pragma once
+#include<iostream>
+#include <vector>
+#include "routing_tuple.h"
+using namespace std;
+class routing_table //simple vector container with specialised ftns
+{
+	
+    vector<routing_tuple> table;
+
+    public:
+    void insert(routing_tuple _tuple)
+    {
+        table.push_back(_tuple);
+    }
+    void display()
+    {
+        cout<<"DEST_PORT\tNUM_HOPS\tROUTER_PORT\tNEXT_PORT\n";
+        for(auto it = table.begin(); it!= table.end(); ++it)
+        {
+            cout<<*it<<"\n";
+        }
+    }
+    int getnext_port(int port)
+    {
+    	for(auto it = table.begin(); it!= table.end(); ++it)
+        {
+            if(it->getdestination_port() == port)
+            	return it->getNext_port();
+        }
+    }
+};
